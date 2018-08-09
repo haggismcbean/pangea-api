15ffa4f6b97af3bed72f3a2a9915e2049ae38e74
Harry <hbishop11@gmail.com>
2018-08-09 22:15:18 +0200

    created Post table, seeder, Post model

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Post;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Let's truncate our existing records to start from scratch.
+        Post::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        // And now, let's create a few posts in our database:
+        for ($i = 0; $i < 50; $i++) {
+            Post::create([
+                'body' => $faker->paragraph,
+                'characterId' => 0,
+                'groupId' => 0
+            ]);
+        }
+    }
+}
