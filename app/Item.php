0b1ce24590da8fca1138ede06e37cee28bbc12ac
Harry <hbishop11@gmail.com>
2019-01-26 18:22:18 +0100

    Altered comments a little

--- app/Item.php
+++ app/Item.php
@@ -21,18 +21,13 @@ class Item extends Model
     }
 
     public function itemDetails() {
-    	if ($this->item_type === 'plant') {
-    		return $this->belongsTo('App\Plant', 'type_id')->first();
-    	}
+        if ($this->item_type === 'plant') {
+            return $this->belongsTo('App\Plant', 'type_id')->first();
+        }
 
         // TODO - seed the wood table
         if ($this->item_type === 'wood') {
             return $this->belongsTo('App\Wood', 'type_id')->first();
-        }
-
-        // TODO - seed the metal table (by hand)
-        if ($this->item_type === 'metal') {
-            return $this->belongsTo('App\Metal', 'type_id')->first();
         }
 
         // TODO - seed the stone table
@@ -40,14 +35,29 @@ class Item extends Model
             return $this->belongsTo('App\Stone', 'type_id')->first();
         }
 
+        // TODO - seed the animal_product table (first create animals though!)
+        if ($this->item_type === 'animal_product') {
+            return $this->belongsTo('App\AnimalProduct', 'type_id')->first();
+        }
+
+        // TODO - seed the metal table (by hand)
+        if ($this->item_type === 'metal') {
+            return $this->belongsTo('App\Metal', 'type_id')->first();
+        }
+
         // TODO - seed the mineral table (by hand)
         if ($this->item_type === 'mineral') {
             return $this->belongsTo('App\Mineral', 'type_id')->first();
         }
 
-        // TODO - seed the animal_product table (first create animals though!)
-        if ($this->item_type === 'animal_product') {
-            return $this->belongsTo('App\AnimalProduct', 'type_id')->first();
+        // TODO - seed the made table (by hand)
+        // TODO - seed the made item recipes table (by hand)
+        // TODO - seed the made item recipe ingredients table (by hand)
+        // It might be useful to just make a little helper webpage for this. And for adding items in general.
+        // Seems less painful than entering things directly into a database.
+        // Bear in mind you want to be able to seed things though
+        if ($this->item_type === 'made_item') {
+            return $this->belongsTo('App\Made', 'type_id')->first();
         }
 
         // TODO - seed the made table (partially by hand - things like bread and such)
@@ -56,13 +66,9 @@ class Item extends Model
             return $this->belongsTo('App\MadeFood', 'type_id')->first();
         }
 
-        // TODO - seed the made table (by hand)
-        // TODO - seed the made item recipes table (by hand)
-        // TODO - seed the made item recipe ingredients table (by hand)
-        if ($this->item_type === 'made') {
-            return $this->belongsTo('App\Made', 'type_id')->first();
-        }
-
         // WHEN ALL OF THE ABOVE IS DONE, WE CAN WORK OUT THE ITEMS TABLE STUFF
+        // I think it might be easier to get all this stuff working if i have a big excel table and then we read the stuff from there
+        // One for each seeded thing
+        // And different rows in it seed different tables.
     }
 }
